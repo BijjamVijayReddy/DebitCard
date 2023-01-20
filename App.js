c9204a580f940eee66f3eee4e96306d095f680aa
Vijay <90137500+BijjamVijayReddy@users.noreply.github.com>
2023-01-20 19:05:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DebitCard from "../src/DebitCard/DebitCard.jsx"
+
+function App() {
+  return (
+    <div>
+    <DebitCard/>
+    </div>
+  )
+}
+
+export default App
